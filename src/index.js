459cbb427790b2c16371187eb74cc3a73b789e38
Jeff Auriemma <bignimbus@users.noreply.github.com>
2022-10-13 00:55:01 -0400

    Reproduce with patch version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { gql, useQuery, ApolloClient, InMemoryCache, ApolloProvider, } from '@apollo/client'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const TEST = gql`
+  query {
+    foobar {
+      id
+      lastOnlineAt
+    }
+  }
+`
+
+function Test() {
+  const { loading } = useQuery(TEST, { fetchPolicy: 'cache-and-network' })
+
+  return <div>loading = {loading ? 'true' : 'false'}</div>
+}
+
+function App() {
+  const [count, setCount] = useState(1)
+
+  return (
+    <div>
+      {Array.from({ length: count }).map((_, i) => <Test key={i} />)}
+
+      <button onClick={() => setCount(count + 1)}>Add</button>
+    </div>
+  )
+}
+
+const client = new ApolloClient({
+  uri: 'https://o5xgyw.sse.codesandbox.io',
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </ApolloProvider>,
+  document.getElementById('root')
+)
